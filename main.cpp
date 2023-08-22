f9e2481d61e0ea48a9f95aba2fad3c7739f50453
HCastillaC <hugocc0113@gmail.com>
2023-08-22 14:06:50 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string backtracking(string reamining_numbers, string set_numbers)
+{
+    //if(set_numbers)
+}
+
 int main()
 {
-    int numbers[4];
+    string numbers = "";
+    char input;
 
-    for(int i = 0; i < 4; i++)
+    while(numbers.size() < 4)
     {
-        cout << "Input number " << i + 1 << ": " << endl;
-        cin >> numbers[i];
-        cout << endl;
+        cout << "Input number " << numbers.size() + 1 << ": ";
+        cin >> input;
+
+        if(input == '0' or input == '1' or input == '2' or input == '3' or input == '4' or input == '5' or
+        input == '6' or input == '7' or input == '8' or input == '9') numbers += input;
     }
+
+    cout << endl << numbers;
+    
+    //backtracking(numbers, )
 
     return 0;
 }
