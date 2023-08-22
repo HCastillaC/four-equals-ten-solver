902c6e9a1e5b7ebd28e9b7ec2594e37f312da076
HCastillaC <hugocc0113@gmail.com>
2023-08-22 14:19:12 +0200

    Backtracking done

--- main.cpp
+++ main.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
-string backtracking(string reamining_numbers, string set_numbers)
+void backtracking(string remaining_numbers, string set_numbers)
 {
-    //if(set_numbers)
+    if(remaining_numbers == "") cout << set_numbers << endl;
+    else
+    {
+        for(int i = 0; i < remaining_numbers.size(); i++)
+        {
+            string temp_remaining_numbers = remaining_numbers;
+            string temp_set_numbers = set_numbers;
+            char selected = remaining_numbers[i];
+
+            temp_remaining_numbers.erase(temp_remaining_numbers.begin() + i);
+            temp_set_numbers += selected;
+
+            backtracking(temp_remaining_numbers, temp_set_numbers);
+        }
+    }
 }
 
 int main()
@@ -21,10 +35,8 @@ int main()
         if(input == '0' or input == '1' or input == '2' or input == '3' or input == '4' or input == '5' or
         input == '6' or input == '7' or input == '8' or input == '9') numbers += input;
     }
-
-    cout << endl << numbers;
     
-    //backtracking(numbers, )
+    backtracking(numbers, "");
 
     return 0;
 }
