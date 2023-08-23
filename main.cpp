2b39d63a799039dcb788bec843185c86aeaa5b46
HCastillaC <hugocc0113@gmail.com>
2023-08-23 14:02:46 +0200

    Loop through all operations

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+void operate(int number[], int operation_priority[], int set_operation[])
+{
+    for(int i = 0; i < 4; i++) cout << number[i];
+    cout << endl;
+    for(int i = 0; i < 3; i++) cout << operation_priority[i];
+    cout << endl;
+    for(int i = 0; i < 3; i++) cout << set_operation[i];
+    cout << endl;
+    cout << endl;
+}
+
 void checking_operations(string str_numbers)
 {
     //Convert to array of ints
@@ -16,13 +27,46 @@ void checking_operations(string str_numbers)
         i++;
     }
 
-    //
+    //Try all the operations
+
+    for(int parenthesis_index = 0; parenthesis_index < 6; parenthesis_index++)
+    {
+        int operation_priority[3] = {0, 0, 0};
+        //* 0: + ; 1: - ; 2: * ; 3: /
+        int set_operation[3] = {0, 0, 0};
+
+        switch(parenthesis_index)
+        {
+            case 0: operation_priority[0] += 2;
+            case 1: operation_priority[1] += 2;
+            case 2: operation_priority[2] += 2; break;
+            case 3: operation_priority[0] += 2;
+            case 4: operation_priority[1] += 2; break;
+            case 5: operation_priority[0] += 2; break;
+        }
+        for(int first_operation_index = 0; first_operation_index < 4; first_operation_index++)
+        {
+            set_operation[0] = first_operation_index;
+            if(first_operation_index == 2) operation_priority[0]++;
+            for(int second_operation_index = 0; second_operation_index < 4; second_operation_index++)
+            {
+                set_operation[1] = second_operation_index;
+                if(second_operation_index == 2) operation_priority[1]++;
+                for(int third_operation_index = 0; third_operation_index < 4; third_operation_index++)
+                {
+                    set_operation[2] = third_operation_index;
+                    if(third_operation_index == 2) operation_priority[2]++;
+                    operate(numbers_int, operation_priority, set_operation);
+                }
+            }
+        }
+    }
 
 }
 
 void backtracking(string remaining_numbers, string set_numbers)
 {
-    if(remaining_numbers == "") cout << set_numbers << endl;
+    if(remaining_numbers == "") checking_operations(set_numbers);
     else
     {
         for(int i = 0; i < remaining_numbers.size(); i++)
